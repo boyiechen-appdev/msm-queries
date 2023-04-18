94f11ec2a6a0b526dbcd80d63b3d9d5893dc6aaf
boyiechen <boyie@uchicago.edu>
2023-04-18 15:19:42 +0000

    build routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def directors
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def directors_youngest
+    render({ :template => "misc_templates/directors_youngest"})
+  end
+
+  def directors_eldest
+    render({ :template => "misc_templates/directors_eldest"})
+  end
+
+  def movies
+    render({ :template => "misc_templates/movies"})
+  end
+  
+  def actors
+    render({ :template => "misc_templates/actors"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "application", :action => "directors" })
+
+  get("/directors/youngest", { :controller => "application", :action => "directors_youngest" })
+  get("/directors/eldest", { :controller => "application", :action => "directors_eldest" })
+
+  get("/movies", { :controller => "application", :action => "movies" })
+
+  get("/actors", { :controller => "application", :action => "actors" })
 end
